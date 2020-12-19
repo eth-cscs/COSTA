1bd72c511cdaab906455c1ebd5343e9823a17f9f
Marko Kabic <marko.kabic@cscs.ch>
2020-12-19 19:17:20 +0100

    Adding the hungarian algorithm.

--- src/costa/grid2grid/ranks_reordering.cpp
+++ src/costa/grid2grid/ranks_reordering.cpp
@@ -1,6 +1,6 @@
 #include <costa/grid2grid/ranks_reordering.hpp>
 #include <algorithm>
-#include <costa/hungarian/hungarian.h>
+#include <costa/hungarian/Hungarian.h>
 
 std::vector<int> costa::optimal_reordering(comm_volume& comm_volume, int n_ranks, bool& reordered) {
     std::vector<bool> visited(n_ranks, false);
@@ -9,14 +9,16 @@ std::vector<int> costa::optimal_reordering(comm_volume& comm_volume, int n_ranks
     std::vector<int> permutation;
     permutation.reserve(n_ranks);
     for (size_t i = 0; i < n_ranks; ++i) {
-        permutation.push_back(i);
+        permutation.push_back(-1);
     }
     reordered = false;
 
-    std::vector<WeightedBipartiteEdge> edges;
-
     std::vector<weighted_edge_t> sorted_edges;
     sorted_edges.reserve(comm_volume.volume.size());
+
+    double max_w = 0;
+
+    std::vector<std::vector<double>> weights(n_ranks, std::vector<double>(n_ranks, 0));
 
     for (const auto& el : comm_volume.volume) {
         auto& e = el.first;
@@ -31,23 +33,22 @@ std::vector<int> costa::optimal_reordering(comm_volume& comm_volume, int n_ranks
         w -= comm_volume.volume[edge_t{src, src}];
         w -= comm_volume.volume[edge_t{dest, dest}];
 
-        if (w > 0)
-        std::cout << src << "->" << dest << "=" << w <<std::endl;
+        max_w = std::max(1.0* w, max_w);
 
-        // truncate to int range
-        w = std::min(w, std::numeric_limits<int>::max());
-        w = std::max(w, std::numeric_limits<int>::min());
-        auto we = WeightedBipartiteEdge(src, dest, -w);
-        edges.push_back(we);
-        auto we2 = WeightedBipartiteEdge(dest, src, -w);
-        edges.push_back(we2);
-
-        sorted_edges.push_back(weighted_edge_t(src, dest, w));
+        sorted_edges.push_back(weighted_edge_t(src, dest, -w));
     }
 
-    std::vector<int> reordering = hungarianMinimumWeightPerfectMatching(n_ranks, edges);
-    reordered = reordering.size() == n_ranks;
-    return reordering;
+    for (auto& e : sorted_edges) {
+        weights[e.src()][e.dest()] = max_w + e.weight();
+        weights[e.dest()][e.src()] = max_w + e.weight();
+    }
+
+    HungarianAlgorithm ha;
+    ha.Solve(weights, permutation);
+
+    // std::vector<int> reordering = hungarianMinimumWeightPerfectMatching(n_ranks, edges);
+    reordered = permutation.size() == n_ranks;
+    return permutation;
 
     // sort the edges by weights (decreasing order)
     std::sort(sorted_edges.rbegin(), sorted_edges.rend());
